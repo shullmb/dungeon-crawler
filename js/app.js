f13e82b14eb12ab39e5a0ef002b62323076673f9
shullmb <shullmb@gmail.com>
2018-06-04 12:04:15 -0700

    moved keypress logic into function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
+// game setup
 const game = document.getElementById('game');
 const ctx = game.getContext('2d');
 game.width = "832";
 game.height = "416";
 
+//globals
 var mage;
 var mush;
 
+// create Crawler object to populate dungeon
 class Crawler {
     constructor(x,y,src) {
         this.x = x;
@@ -13,6 +16,7 @@ class Crawler {
         this.src = src;
         this.width = 32;
         this.height = 32;
+        this.alive = true;
     }
 
     render() {
@@ -22,10 +26,40 @@ class Crawler {
     }
 }
 
+// function to detect contact with monster -- hard coding in mushroom placeholder for development
+const detectEncounter = () => {
+    if (mage.x < mush.x + mush.width
+        && mage.x + mage.width > mush.x
+        && mage.y < mush.y + mush.height
+        && mage.y + mage.height > mush.y) {
+        console.log('Prepare for BATTTLE!')
+        // trigger encounter screen
+    }
+}
+
+const keyInputHandler = (e) => {
+    switch(true) {
+        case (e.keyCode === 87 || e.keyCode === 38):
+            mage.y -= 5;
+            break;
+        case (e.keyCode === 83 || e.keyCode === 40):
+            mage.y += 5;
+            break;
+        case (e.keyCode === 65 || e.keyCode === 37):
+            mage.x -= 5;
+            break;
+        case (e.keyCode === 68 || e.keyCode === 39):
+            mage.x += 5;
+            break;
+    }
+}
+
+// function for game loop
 const gameLoop = () => {
     ctx.clearRect(0,0,game.width,game.height);
     mage.render();
     mush.render();
+    detectEncounter();
 }
 
 
@@ -37,22 +71,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     setInterval(gameLoop, 60);
 
-    document.addEventListener('keydown', function(e){
-        switch(true) {
-            case (e.keyCode === 38):
-                mage.y -= 5;
-                break;
-            case (e.keyCode === 40):
-                mage.y += 5;
-                break;
-            case (e.keyCode === 37):
-                mage.x -= 5;
-                break;
-            case (e.keyCode === 39):
-                mage.x += 5;
-                break;
-        }
-    })
+    document.addEventListener('keydown', keyInputHandler);
 
 
 
