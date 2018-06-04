2c73446866cd59e1af7af75e57720bbd62e55fe4
shullmb <shullmb@gmail.com>
2018-06-04 11:10:10 -0700

    more inital setup

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
 const game = document.getElementById('game');
 const ctx = game.getContext('2d');
+game.width = "832";
+game.height = "416";
 var img;
 var mage;
 var mush;
 
 img = new Image();
-img.src = '../img/plc-mage.png'
+img.src = '../img/plc-mage-32.png'
+
+class Crawler {
+    constructor(x,y,src) {
+        this.x = x;
+        this.y = y;
+        this.src = src;
+        this.width = 32;
+        this.height = 32;
+    }
+
+    render() {
+        ctx.drawImage(this.src,this.x,this.y,this.width,this.height);
+    }
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log('craaaawwwwwwl');
 
-    ctx.drawImage(img, 10,10, 64, 64);
+    ctx.drawImage(img, 10,10, 32,32);
+
 
 
 
