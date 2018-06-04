99eee9cc3cfb2f61a35991113a3b9011e45882a0
shullmb <shullmb@gmail.com>
2018-06-04 13:43:24 -0700

    start/stop interval debugging

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 // game setup
 const game = document.getElementById('game');
 const ctx = game.getContext('2d');
+var loopHandle;
+var dungeonLoopRunning = true;
 game.width = "832";
 game.height = "416";
+
+// DOM hooks
+const topRight = document.getElementById('top-right');
 
 //globals
 var mage;
@@ -33,45 +38,71 @@ const detectEncounter = () => {
         && mage.y < mush.y + mush.height
         && mage.y + mage.height > mush.y) {
         console.log('Prepare for BATTTLE!')
-        // trigger encounter screen
+        // trigger encounter screen as soon as i know how
+        mush.alive = false; 
+        ctx.save();
+        dungeonLoopRunning = false;
     }
 }
 
 const keyInputHandler = (e) => {
+    // console.log(e.keyCode);
     switch(true) {
         case (e.keyCode === 87 || e.keyCode === 38):
-            mage.y -= 5;
+            mage.y -= 10;
             break;
         case (e.keyCode === 83 || e.keyCode === 40):
-            mage.y += 5;
+            mage.y += 10;
             break;
         case (e.keyCode === 65 || e.keyCode === 37):
-            mage.x -= 5;
+            mage.x -= 10;
             break;
         case (e.keyCode === 68 || e.keyCode === 39):
-            mage.x += 5;
+            mage.x += 10;
+            break;
+        // restart loop test
+        case (e.keyCode === 49):
+            restart();
             break;
     }
 }
 
-// function for game loop
-const gameLoop = () => {
+// function for dungeoneering loop
+const dungeonLoop = () => { 
+    detectEncounter();
     ctx.clearRect(0,0,game.width,game.height);
     mage.render();
-    mush.render();
-    detectEncounter();
+    // track position during development/debug -- using innerHTML temporarily
+    topRight.innerHTML = "<h3>x:" + mage.x + " y:" + mage.y + "</h3>";
+
+    if (mush.alive) {
+        mush.render() 
+    }
+}
+
+// utility function to test game state on restart
+const restart = () => {
+    ctx.restore();
+    dungeonLoopRunning = true;
 }
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    console.log('craaaawwwwwwl');
 
+    // create crawlers
     mage = new Crawler(10, 10,'../img/plc-mage-32.png');
     mush = new Crawler(200, 50,'../img/wandering_mushroom_new.png');
 
-    setInterval(gameLoop, 60);
-
+    // set up event listener for keyepress
     document.addEventListener('keydown', keyInputHandler);
+    
+    // start game loop
+    loopHandle = setInterval( function() {
+        if (dungeonLoopRunning == true) {
+            dungeonLoop();
+        } 
+    }, 60);
+    
 
 
 
