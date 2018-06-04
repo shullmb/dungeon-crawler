22d91a6da4d7ef8927ac2809295181b496e7914e
shullmb <shullmb@gmail.com>
2018-06-04 10:37:22 -0700

    added image preloading div

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const game = document.getElementById('game');
+const ctx = game.getContext('2d');
+var img;
+var mage;
+var mush;
+
+img = new Image();
+img.src = '../img/plc-mage.png'
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('craaaawwwwwwl');
+
+    ctx.drawImage(img, 10,10, 64, 64);
+
+
+
+
+
+
+})
